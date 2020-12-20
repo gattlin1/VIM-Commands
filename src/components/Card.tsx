d5377b131cd0e9c8480920c198b699e9e5301072
gattlin1 <gattlin1@live.missouristate.edu>
2020-12-20 15:58:48 -0500

    Messing with props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './Card.scss'
+import React from 'react';
+import './Card.scss';
 
-export default function Card(props: any) {
-    return (
-        <div className="Card">
-            <h3>{props.command}</h3>
-            <img src="{props.example}" alt="" />
-            <p>{props.description}</p>
-        </div>
-    )
+type CardProps = {
+  command: string;
+  description: string;
+};
+
+function Card(props: CardProps) {
+  return (
+    <div className='Card'>
+      <h3>{props.command}</h3>
+      <img src='{props.example}' alt='' />
+      <p>{props.description}</p>
+    </div>
+  );
 }
+
+export default Card;
